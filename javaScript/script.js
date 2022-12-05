8d27325b99e09b4ce0ed4f27b3c0bc406f4726ef
mahmoud essam <vvmahmoud159@gmail.com>
2022-12-05 15:26:55 +0200

    first commit

---
+++ javaScript/script.js
@@ -0,0 +1,5 @@
+window.alert('holle');
+document.write("<h2>mahmoud</h2>");
+console.log("mahmoud");
+console.log( typeof("mahmoud"));
+console.log(typeof{name: "mahmoud", age:158 , country: "sk"});
